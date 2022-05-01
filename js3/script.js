00f309a8324dbdc17decfd632a9107fce8fb3ea1
PetrenchukM <petrenchuk.mykhailo@kmrf.kiev.ua>
2022-05-01 17:17:17 +0300

    js3

---
+++ js3/script.js
@@ -0,0 +1,21 @@
+function task1 ()
+{
+let sentence = prompt("Введіть слово: ");
+
+document.write(sentence);
+}
+function task2 ()
+{
+let sentenc = prompt("Введіть слово: ");
+document.write('<i>' + sentenc + '</i>');
+}
+function task3 ()
+{
+    let str = 'work';
+    document.write('<i>' + str + '</i>');
+}
+function task4 ()
+{
+
+document.write(1+'<br>'+2+'<br>'+3+'<br>'+4+'<br>'+5);
+}
